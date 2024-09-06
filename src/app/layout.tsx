63607e01f04a6df41c14b25dc5c666b738684a94
Jay <jay.sanjay.karia@gmail.com>
2024-09-06 09:25:23 +0530

    chore: initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Starbase Admin",
+  description: "⚙ E-Commerce admin panel",
+  icons: [
+    {
+      href: "/logo.png",
+      url: "/logo.png",
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("h-full w-full", inter.className)}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+  return (
+    <div>
+      Hello Starbase Admin
+      <Button>Hello</Button>
+    </div>
+  );
+}
